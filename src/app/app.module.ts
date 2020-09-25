fe14503bf29b701dd9026db49beb142e03d866f0
GibbyStack <carlosorozco4565@gmail.com>
2020-09-25 18:03:30 -0500

    admin pro 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NopagefoundComponent } from './page/nopagefound/nopagefound.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeadbarComponent } from './shared/headbar/headbar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    SidebarComponent,
+    HeadbarComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule
