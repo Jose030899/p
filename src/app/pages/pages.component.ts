1505181a3adcc71cb0854e82292efbc4d31606d5
Carlos Fernando Orozco Solis <carlosorozco4565@gmail.com>
2020-10-05 20:52:47 -0500

    casi chidori progress

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class PagesComponent implements OnInit {
 
   constructor() { }
+  year = new Date().getFullYear();
 
   ngOnInit(): void {
   }
