04d3abb6cac2c82c5b291ef91224b6e2384e0c15
Carlos Fernando Orozco Solis <carlosorozco4565@gmail.com>
2020-10-08 13:11:27 -0500

    Progress Component Chidori

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -7,6 +7,7 @@ import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { ComponentsModule } from '../components/components.module';
 
 
 
@@ -24,7 +25,7 @@ import { FormsModule } from '@angular/forms';
     PagesComponent
   ],
   imports: [
-    CommonModule, SharedModule, RouterModule, FormsModule
+    CommonModule, SharedModule, RouterModule, FormsModule, ComponentsModule
   ]
 })
 export class PagesModule {}
