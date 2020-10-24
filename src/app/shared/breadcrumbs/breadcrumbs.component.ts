1e85cbc5e9b9ad0fe2ecfcc6f6824a853e427b09
Jose Maria Orozco Ramirez <LRS17110068@purisima.tecnm.mx>
2020-10-23 23:19:57 -0500

    Porfas No falles

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute, ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnDestroy {
+  public titulo: string;
+  public tituloSubs$: Subscription;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.tituloSubs$ = this.getArgumentosRuta().subscribe(({ titulo }) => {
+      this.titulo = titulo;
+      document.title = `Conec v5 - ${titulo}`;
+    });
+  }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
   }
 
+  getArgumentosRuta() {
+    return this.router.events.pipe(
+      filter((event) => event instanceof ActivationEnd),
+      filter((event: ActivatedRoute) => event.snapshot.firstChild === null),
+      map((event: ActivationEnd) => event.snapshot.data)
+    );
+  }
 }
